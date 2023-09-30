1bc0dbb4f1e9d6b2686a95de3aa0de7c915ca9db
Jeon <gino9940@gmail.com>
2023-09-30 18:47:15 +0900

    feat: format font-size for rem

--- src/assets/styles/global.tsx
+++ src/assets/styles/global.tsx
@@ -1,8 +1,18 @@
 import { createGlobalStyle } from 'styled-components';
 import normalize from 'styled-normalize';
 
-const GlobalStyle = createGlobalStyle`
-    ${normalize}
+const styled = { createGlobalStyle };
+
+const GlobalStyle = styled.createGlobalStyle`
+  ${normalize}
+
+  html {
+    font-size: 62.5%;
+  }
+
+  body {
+    background-color: #f6f6f6;
+  }
 `;
 
 export default GlobalStyle;
