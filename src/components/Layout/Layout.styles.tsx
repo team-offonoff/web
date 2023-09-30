da3c8ad6b5f6cc617e53e88a29a0160fd53e11b1
Jeon <gino9940@gmail.com>
2023-09-30 18:47:20 +0900

    feat: add Main container

--- src/components/Layout/Layout.styles.tsx
+++ src/components/Layout/Layout.styles.tsx
@@ -1,3 +1,14 @@
 import { styled } from 'styled-components';
 
+export const Main = styled.main`
+  overflow-y: scroll;
+  position: relative;
+  max-width: 512px;
+  height: 100vh;
+  background-color: #ffffff;
+  margin: 0 auto;
+`;
+
 export const Header = styled.header``;
+
+export const NavigationContainer = styled.div``;
--- src/components/Layout/Layout.tsx
+++ src/components/Layout/Layout.tsx
@@ -1,17 +1,17 @@
 import React from 'react';
 import { Outlet } from 'react-router-dom';
 import BottomNavigation from '../BottomNavigation/BottomNavigation';
-import { Header } from './Layout.styles';
+import { Header, Main } from './Layout.styles';
 
 interface LayoutProps {}
 
 const Layout = ({}: LayoutProps) => {
   return (
-    <main>
+    <Main>
       <Header></Header>
       <Outlet />
       <BottomNavigation />
-    </main>
+    </Main>
   );
 };
 
