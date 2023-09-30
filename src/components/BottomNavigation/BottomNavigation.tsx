35a5a24d975b5d2d83f3ec569149aa43c27475c5
Jeon <gino9940@gmail.com>
2023-09-30 11:36:04 +0900

    feat: impl BottomNavigation

--- src/components/BottomNavigation/BottomNavigation.tsx
+++ src/components/BottomNavigation/BottomNavigation.tsx
@@ -1,11 +1,24 @@
 import React from 'react';
 import { NavLink } from 'react-router-dom';
+import { ABIcn, PlusBoxIcn, ProfileIcn, SelectedHomeIcn } from '../../assets/icons';
+import { Container } from './BottomNavigation.styles';
 
 const BottomNavigation = () => {
   return (
-    <nav>
-      <NavLink to={'/'}>Home</NavLink>
-    </nav>
+    <Container>
+      <NavLink to={'/'}>
+        <SelectedHomeIcn />
+      </NavLink>
+      <NavLink to={'/ab'}>
+        <ABIcn />
+      </NavLink>
+      <NavLink to={'/write'}>
+        <PlusBoxIcn />
+      </NavLink>
+      <NavLink to={'/profile'}>
+        <ProfileIcn />
+      </NavLink>
+    </Container>
   );
 };
 
