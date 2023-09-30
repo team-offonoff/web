4b339fb579a5058d0267613baaad09cf2da6476d
Jeon <gino9940@gmail.com>
2023-09-30 11:35:48 +0900

    feat: add svg assets

---
+++ src/assets/icons/index.tsx
@@ -0,0 +1,4 @@
+export { ReactComponent as SelectedHomeIcn } from './home-selected.svg';
+export { ReactComponent as ABIcn } from './ab.svg';
+export { ReactComponent as PlusBoxIcn } from './plus-box.svg';
+export { ReactComponent as ProfileIcn } from './profile.svg';
