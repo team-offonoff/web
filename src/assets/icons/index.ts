effc05fbead5a1b8313e7a57d17002b1aa5c70f3
chaeyoung103 <thdcodud103@naver.com>
2023-10-02 18:55:43 +0900

    feat: #7 add alarm icon

--- src/assets/icons/index.ts
+++ src/assets/icons/index.ts
@@ -2,5 +2,7 @@ import SelectedHomeIcon from './home-selected.svg?react';
 import ABIcon from './ab.svg?react';
 import PlusBoxIcon from './plus-box.svg?react';
 import ProfileIcon from './profile.svg?react';
+import AlarmIcon from './alarm-default.svg?react';
+import NewAlarmIcon from './alarm-new.svg?react';
 
-export { SelectedHomeIcon, ABIcon, PlusBoxIcon, ProfileIcon };
+export { SelectedHomeIcon, ABIcon, PlusBoxIcon, ProfileIcon, AlarmIcon, NewAlarmIcon };
