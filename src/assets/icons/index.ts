256b70375a01700f2d5d43a8cee41ac7fc0962ba
chaeyoung103 <thdcodud103@naver.com>
2023-10-03 21:23:13 +0900

    feat: #7 add next icon

--- src/assets/icons/index.ts
+++ src/assets/icons/index.ts
@@ -4,5 +4,6 @@ import PlusBoxIcon from './plus-box.svg?react';
 import ProfileIcon from './profile.svg?react';
 import AlarmIcon from './alarm-default.svg?react';
 import NewAlarmIcon from './alarm-new.svg?react';
+import NextIcon from './next.svg?react';
 
-export { SelectedHomeIcon, ABIcon, PlusBoxIcon, ProfileIcon, AlarmIcon, NewAlarmIcon };
+export { SelectedHomeIcon, ABIcon, PlusBoxIcon, ProfileIcon, AlarmIcon, NewAlarmIcon, NextIcon };
