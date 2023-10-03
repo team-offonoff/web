27b3607fe9e1a2120e417cee448471f3a00dba00
chaeyoung103 <thdcodud103@naver.com>
2023-10-03 21:26:15 +0900

    feat: #7 style home 기본 ui

--- src/routes/Home/Home.styles.tsx
+++ src/routes/Home/Home.styles.tsx
@@ -4,3 +4,123 @@ export const Container = styled.div`
   height: 100%;
   background-color: #242036;
 `;
+
+export const AlarmButton = styled.div`
+  width: 24px;
+  height: 24px;
+`;
+
+export const BestTopicCotainer = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  width: 100%;
+  height: 28px;
+`;
+
+export const BestTopicTitle = styled.div`
+  font-size: 2rem;
+  font-weight: 400;
+  color: #a46ff3;
+  text-align: center;
+`;
+
+export const TopicContainer = styled.div`
+  box-sizing: border-box;
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+  width: 100%;
+  height: 68px;
+  padding: 0 12px;
+  margin-top: 12px;
+`;
+
+export const Topic = styled.div`
+  width: 170px;
+  font-size: 2.4rem;
+  font-weight: 600;
+  line-height: 140%;
+  color: #fff;
+  text-align: center;
+  letter-spacing: 0.2px;
+`;
+
+export const NavigateButton = styled.button`
+  width: 40px;
+  height: 40px;
+  background-color: transparent;
+`;
+
+export const UnderlineTextButtonContainer = styled.button`
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  width: 100%;
+  margin-top: 4px;
+`;
+
+export const UnderlineTextButton = styled.button`
+  font-size: 1.3rem;
+  font-weight: 400;
+  color: rgb(255 255 255 / 40%);
+  text-align: center;
+  text-decoration: underline;
+`;
+
+export const TimerContainer = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  width: 100%;
+  height: 37px;
+  margin-top: 37px;
+`;
+
+export const Timer = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  width: 115px;
+  height: 37px;
+  font-size: 1.5rem;
+  font-weight: 700;
+  color: rgb(255 255 255 / 40%);
+  text-align: center;
+  background-color: #3c3457;
+  border-radius: 50px;
+`;
+
+export const SelectContainer = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  width: 100%;
+  height: 188px;
+  margin-top: 37px;
+`;
+
+export const UserInfoContainer = styled.div`
+  display: flex;
+  gap: 8px;
+  align-items: center;
+  justify-content: center;
+  width: 100%;
+  height: 25px;
+  margin-top: 49px;
+`;
+
+export const UserProfileImage = styled.div`
+  width: 20px;
+  height: 20px;
+  background-color: #bcbcbc;
+  border-radius: 50%;
+`;
+
+export const UserProfileName = styled.div`
+  margin-top: 2.5px;
+  font-size: 1.8rem;
+  font-weight: 400;
+  color: #fff;
+  text-align: center;
+`;
--- src/routes/Home/Home.tsx
+++ src/routes/Home/Home.tsx
@@ -1,8 +1,50 @@
 import React from 'react';
-import { Container } from './Home.styles';
+import {
+  BestTopicCotainer,
+  BestTopicTitle,
+  Container,
+  NavigateButton,
+  TimerContainer,
+  Timer,
+  Topic,
+  TopicContainer,
+  UnderlineTextButton,
+  UnderlineTextButtonContainer,
+  SelectContainer,
+  UserInfoContainer,
+  UserProfileImage,
+  UserProfileName,
+} from './Home.styles';
+import { NextIcon } from '../../assets/icons';
 
 const Home = () => {
-  return <Container>Home</Container>;
+  return (
+    <Container>
+      <BestTopicCotainer>
+        <BestTopicTitle>실시간 인기토픽</BestTopicTitle>
+      </BestTopicCotainer>
+      <TopicContainer>
+        <NavigateButton></NavigateButton>
+        <Topic>10년전 또는 후로 갈 수 있다면?</Topic>
+        <NavigateButton>
+          <NextIcon />
+        </NavigateButton>
+      </TopicContainer>
+      <UnderlineTextButtonContainer>
+        <UnderlineTextButton>이런 토픽은 안볼래요</UnderlineTextButton>
+      </UnderlineTextButtonContainer>
+      <TimerContainer>
+        <Timer>01 : 00 : 00</Timer>
+      </TimerContainer>
+      <SelectContainer></SelectContainer>
+      <UserInfoContainer>
+        <UserProfileImage></UserProfileImage>
+        <UserProfileName>
+          <b>체리체리체리체리체리체리</b> 님의 토픽
+        </UserProfileName>
+      </UserInfoContainer>
+    </Container>
+  );
 };
 
 export default Home;
