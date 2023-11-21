1aeaf1f491e2d6868549b4a3c3f7356e8f52ce22
Jeon <gino9940@gmail.com>
2023-11-21 17:31:55 +0900

    feat: useFunnel in signup

--- src/routes/Auth/signup/Signup.tsx
+++ src/routes/Auth/signup/Signup.tsx
@@ -1,19 +1,28 @@
 import React, { useState } from 'react';
 
+import useFunnel from '@hooks/useFunnel';
+
 import 가입성공 from './가입성공';
 import 약관동의 from './약관동의';
 import 정보입력 from './정보입력';
 
 const Signup = () => {
   const [registerData, setRegisterData] = useState();
-  const [step, setStep] = useState<'정보입력' | '약관동의' | '가입성공'>('정보입력');
+  const [Funnel, setStep] = useFunnel(['가입방식' | '주민번호' | '집주소' | '가입성공']);
 
   return (
-    <main>
-      {step === '정보입력' && <정보입력 />}
-      {step === '약관동의' && <약관동의 />}
-      {step === '가입성공' && <가입성공 />}
-    </main>
+    <></>
+    // <Funnel>
+    //   <Funnel.Step name="정보입력">
+    //     <정보입력 />
+    //   </Funnel.Step>
+    //   <Funnel.Step name="약관동의">
+    //     <약관동의 />
+    //   </Funnel.Step>
+    //   <Funnel.Step name="가입성공">
+    //     <가입성공 />
+    //   </Funnel.Step>
+    // </Funnel>
   );
 };
 
