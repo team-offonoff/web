ab380cbc99c2a6204da828684974d346da11dd92
Jeon <gino9940@gmail.com>
2023-12-01 16:29:04 +0900

    fix: move timer related styled to Timer component

--- src/components/TopicCard/TopicCard.styles.tsx
+++ src/components/TopicCard/TopicCard.styles.tsx
@@ -71,29 +71,6 @@ export const SkipButton = styled.button`
   cursor: pointer;
 `;
 
-export const TimerContainer = styled.div`
-  display: flex;
-  align-items: center;
-  justify-content: center;
-  width: 100%;
-  height: 37px;
-  margin-top: 37px;
-`;
-
-export const Timer = styled.div<{ isLessThanOneHour: boolean }>`
-  display: flex;
-  align-items: center;
-  justify-content: center;
-  width: 115px;
-  height: 37px;
-  font-size: 1.5rem;
-  font-weight: 700;
-  color: ${(props) => (props.isLessThanOneHour ? '#3C3457' : 'rgb(255 255 255 / 40%)')};
-  text-align: center;
-  background-color: ${(props) => (props.isLessThanOneHour ? props.theme.colors.purple : '#3c3457')};
-  border-radius: 50px;
-`;
-
 export const SelectContainer = styled.div`
   display: flex;
   align-items: center;
