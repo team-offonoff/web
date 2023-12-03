5f7cd5ccb38d1cdaa2baf8504edcd0a5015cc4f0
chaeyoung103 <thdcodud103@naver.com>
2023-12-03 19:35:14 +0900

    feat: meatballIcon, arrowIcon 추가

--- src/assets/icons/index.ts
+++ src/assets/icons/index.ts
@@ -5,9 +5,12 @@ import AppleLoginIcon from './apple-login.svg?react';
 import GoogleLoginIcon from './google-login.svg?react';
 import SelectedHomeIcon from './home-selected.svg?react';
 import KakaoLoginIcon from './kakao-login.svg?react';
+import LeftDoubleArrowIcon from './left-double-arrow.svg?react';
+import MeatballIcon from './meatball.svg?react';
 import PlusBoxIcon from './plus-box.svg?react';
 import ProfileIcon from './profile.svg?react';
 import RightChevronIcon from './right-chevron.svg?react';
+import RightDoubleArrowIcon from './right-double-arrow.svg?react';
 import WriteBoxIcon from './write-box.svg?react';
 
 export {
@@ -22,4 +25,7 @@ export {
   KakaoLoginIcon,
   AppleLoginIcon,
   GoogleLoginIcon,
+  LeftDoubleArrowIcon,
+  RightDoubleArrowIcon,
+  MeatballIcon,
 };
