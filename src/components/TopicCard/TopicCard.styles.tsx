11013a5e8b4ee3fe1178baec16ebf8c05ff8429b
chaeyoung103 <thdcodud103@naver.com>
2023-12-03 19:36:23 +0900

    feat: 홈화면 수정사항에 따라 ui 수정 및 comment 컴포넌트 추가

--- src/components/TopicCard/TopicCard.styles.tsx
+++ src/components/TopicCard/TopicCard.styles.tsx
@@ -1,5 +1,7 @@
 import { styled } from 'styled-components';
 
+import { colors } from '@styles/theme';
+
 export const Container = styled.div`
   height: 100%;
   background-color: ${(props) => props.theme.colors.navy};
@@ -8,6 +10,7 @@ export const Container = styled.div`
 export const TopicCardContainer = styled.div`
   display: flex;
   flex-direction: column;
+  align-items: center;
   height: 100%;
 `;
 
@@ -22,7 +25,7 @@ export const BestTopicCotainer = styled.div`
   align-items: center;
   justify-content: center;
   width: 100%;
-  height: 28px;
+  height: 25px;
 `;
 
 export const TopicContainer = styled.div`
@@ -33,7 +36,7 @@ export const TopicContainer = styled.div`
   width: 100%;
   height: 68px;
   padding: 0 12px;
-  margin-top: 12px;
+  margin-top: 20px;
 `;
 
 export const Topic = styled.div`
@@ -46,31 +49,14 @@ export const Topic = styled.div`
   letter-spacing: 0.2px;
 `;
 
-export const SkipButtonContainer = styled.div`
-  display: flex;
-  align-items: center;
-  justify-content: center;
-  width: 100%;
-  margin-top: 4px;
-  cursor: pointer;
-`;
-
-export const SkipButton = styled.button`
-  font-size: 1.3rem;
-  font-weight: 400;
-  color: rgb(255 255 255 / 40%);
-  text-align: center;
-  text-decoration: underline;
-  cursor: pointer;
-`;
-
 export const SelectContainer = styled.div`
   display: flex;
   align-items: center;
   justify-content: center;
   width: 100%;
-  height: 188px;
-  margin-top: 37px;
+  height: 220px;
+  margin-top: 20px;
+  margin-bottom: 7px;
 `;
 
 export const UserInfoContainer = styled.div`
@@ -80,12 +66,109 @@ export const UserInfoContainer = styled.div`
   justify-content: center;
   width: 100%;
   height: 25px;
-  margin-top: 49px;
+  margin-top: 4px;
 `;
 
 export const UserProfileImage = styled.div`
   width: 20px;
   height: 20px;
-  background-color: #bcbcbc;
+  background-color: #555;
   border-radius: 50%;
+`;
+
+export const SelectTextContainer = styled.div`
+  display: flex;
+  gap: 6px;
+  align-items: center;
+  justify-content: space-between;
+  height: 25px;
+  margin: 4px 132px 0;
+`;
+
+export const CommentContainer = styled.div`
+  box-sizing: border-box;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: flex-start;
+  width: 100%;
+  padding: 41px 20px 0;
+`;
+
+export const CommentHeader = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+  width: 100%;
+  height: 22px;
+`;
+
+export const KeywordContainer = styled.div`
+  display: flex;
+  gap: 6px;
+  align-items: center;
+  justify-content: flex-start;
+`;
+
+export const CommentInfoContainer = styled.div`
+  display: flex;
+  gap: 12px;
+  align-items: center;
+  justify-content: flex-start;
+  width: 100%;
+  height: 40px;
+  padding: 0 16px;
+  margin-top: 11px;
+  background-color: #64519b;
+  border-radius: 10px 10px 0 0;
+`;
+
+export const Comment = styled.div`
+  position: relative;
+  box-sizing: border-box;
+  display: flex;
+  gap: 10px;
+  align-items: center;
+  justify-content: flex-start;
+  width: 100%;
+  height: 57px;
+  padding: 0 16px;
+  background-color: ${colors.navy2};
+  border-radius: 0 0 10px 10px;
+`;
+
+export const Blur = styled.div<{ isVote: boolean }>`
+  box-sizing: border-box;
+  display: flex;
+  gap: 10px;
+  align-items: center;
+  justify-content: flex-start;
+  width: 100%;
+  height: 29px;
+  background-color: transparent;
+  filter: ${(props) => (!props.isVote ? 'blur(2px)' : 'blur(0px)')};
+`;
+
+export const HighlightText = styled.span`
+  color: white;
+`;
+
+export const CommentButton = styled.button`
+  position: absolute;
+  top: 50%;
+  left: 50%;
+  display: inline-flex;
+  align-items: center;
+  justify-content: center;
+  width: 140px;
+  height: 30px;
+  padding: 4px 14px;
+  font-size: 1.5rem;
+  font-weight: 700;
+  line-height: 0%;
+  color: ${colors.white};
+  text-align: center;
+  background-color: ${colors.purple};
+  border-radius: 91px;
+  transform: translate(-50%, -50%);
 `;
--- src/components/TopicCard/TopicCard.tsx
+++ src/components/TopicCard/TopicCard.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { useNavigate } from 'react-router';
 
 import Text from '@components/Text/Text';
@@ -6,30 +6,35 @@ import Timer from '@components/Timer/Timer';
 
 import { colors } from '@styles/theme';
 
+import { LeftDoubleArrowIcon, MeatballIcon, RightDoubleArrowIcon } from '@icons/index';
+
 import {
   BestTopicCotainer,
   TopicContainer,
   Topic,
-  SkipButtonContainer,
-  SkipButton,
   SelectContainer,
   UserInfoContainer,
   UserProfileImage,
   TopicCardContainer,
+  SelectTextContainer,
+  CommentContainer,
+  CommentHeader,
+  KeywordContainer,
+  CommentInfoContainer,
+  Comment,
+  HighlightText,
+  Blur,
+  CommentButton,
 } from './TopicCard.styles';
 
 const TopicCard = () => {
-  const profileName = '체리체리체리체리';
+  const profileName = '닉네임닉네임';
   const topic = '10년전 또는 후로 갈 수 있다면?';
   const endTime = new Date();
+  const [isVote, SetIsVote] = useState(false);
   endTime.setHours(endTime.getHours() + 4);
 
   const navigate = useNavigate();
-
-  const handleSkipButton = () => {
-    /*현재토픽 skip 후 다음토픽 으로 이동*/
-    navigate('/login');
-  };
 
   return (
     <TopicCardContainer>
@@ -41,17 +46,54 @@ const TopicCard = () => {
       <TopicContainer>
         <Topic>{topic}</Topic>
       </TopicContainer>
-      <SkipButtonContainer>
-        <SkipButton onClick={handleSkipButton}>이런 토픽은 안볼래요</SkipButton>
-      </SkipButtonContainer>
-      <Timer endTime={endTime.getTime()} />
-      <SelectContainer></SelectContainer>
       <UserInfoContainer>
         <UserProfileImage></UserProfileImage>
-        <Text size={18}>
-          <b>{profileName}</b> 님의 토픽
+        <Text size={14} weight={'regular'} color={colors.white_60}>
+          {profileName}
         </Text>
       </UserInfoContainer>
+      <SelectContainer></SelectContainer>
+      <Timer endTime={endTime.getTime()} />
+      <SelectTextContainer>
+        <LeftDoubleArrowIcon />
+        <Text size={14} weight={'regular'} color={colors.white_40}>
+          밀어서 선택하기
+        </Text>
+        <RightDoubleArrowIcon />
+      </SelectTextContainer>
+      <CommentContainer>
+        <CommentHeader>
+          <KeywordContainer>
+            <Text size={13} weight={'regular'} color={colors.purple}>
+              A 사이드
+            </Text>
+            <Text size={14} weight={'regular'} color={colors.white_20}>
+              |
+            </Text>
+            <Text size={13} weight={'regular'} color={colors.white_60}>
+              대표키워드요
+            </Text>
+          </KeywordContainer>
+          <MeatballIcon />
+        </CommentHeader>
+        <CommentInfoContainer>
+          <Text size={14} weight={600} color={colors.white_60}>
+            <HighlightText>1천개</HighlightText>의 댓글
+          </Text>
+          <Text size={14} weight={600} color={colors.white_60}>
+            <HighlightText>1.2천명</HighlightText>이 선택했어요
+          </Text>
+        </CommentInfoContainer>
+        <Comment>
+          <Blur isVote={isVote}>
+            <UserProfileImage />
+            <Text size={15} weight={'regular'} color={colors.white}>
+              나는 10년 전 과거로 가서 주식...
+            </Text>
+          </Blur>
+          {!isVote && <CommentButton>선택하고 댓글 보기</CommentButton>}
+        </Comment>
+      </CommentContainer>
     </TopicCardContainer>
   );
 };
