aebb2dfef5bdf6e48b5c0ff89554090fe5afcb85
Jeon <gino9940@gmail.com>
2023-12-04 22:14:54 +0900

    refactor: separate CommentBox from TopicCard

--- src/components/TopicCard/TopicCard.tsx
+++ src/components/TopicCard/TopicCard.tsx
@@ -1,6 +1,7 @@
 import React, { useState } from 'react';
 import { useNavigate } from 'react-router';
 
+import CommentBox from '@components/CommentBox/CommentBox';
 import Text from '@components/Text/Text';
 import Timer from '@components/Timer/Timer';
 
@@ -17,21 +18,14 @@ import {
   UserProfileImage,
   TopicCardContainer,
   SelectTextContainer,
-  CommentContainer,
-  CommentHeader,
-  KeywordContainer,
-  CommentInfoContainer,
-  Comment,
-  HighlightText,
-  Blur,
-  CommentButton,
 } from './TopicCard.styles';
 
 const TopicCard = () => {
+  const [hasVoted, setHasVoted] = useState(false);
+
   const profileName = '닉네임닉네임';
   const topic = '10년전 또는 후로 갈 수 있다면?';
   const endTime = new Date();
-  const [isVote, SetIsVote] = useState(false);
   endTime.setHours(endTime.getHours() + 4);
 
   const navigate = useNavigate();
@@ -61,39 +55,14 @@ const TopicCard = () => {
         </Text>
         <RightDoubleArrowIcon />
       </SelectTextContainer>
-      <CommentContainer>
-        <CommentHeader>
-          <KeywordContainer>
-            <Text size={13} weight={'regular'} color={colors.purple}>
-              A 사이드
-            </Text>
-            <Text size={14} weight={'regular'} color={colors.white_20}>
-              |
-            </Text>
-            <Text size={13} weight={'regular'} color={colors.white_60}>
-              대표키워드요
-            </Text>
-          </KeywordContainer>
-          <MeatballIcon />
-        </CommentHeader>
-        <CommentInfoContainer>
-          <Text size={14} weight={600} color={colors.white_60}>
-            <HighlightText>1천개</HighlightText>의 댓글
-          </Text>
-          <Text size={14} weight={600} color={colors.white_60}>
-            <HighlightText>1.2천명</HighlightText>이 선택했어요
-          </Text>
-        </CommentInfoContainer>
-        <Comment>
-          <Blur isVote={isVote}>
-            <UserProfileImage />
-            <Text size={15} weight={'regular'} color={colors.white}>
-              나는 10년 전 과거로 가서 주식...
-            </Text>
-          </Blur>
-          {!isVote && <CommentButton>선택하고 댓글 보기</CommentButton>}
-        </Comment>
-      </CommentContainer>
+      <CommentBox
+        hasVoted={hasVoted}
+        side={'A'}
+        keyword={'키워드'}
+        commentCount={0}
+        voteCount={0}
+        topComment={'top comment here!'}
+      />
     </TopicCardContainer>
   );
 };
