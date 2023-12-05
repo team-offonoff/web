ada98710a421b26d62ecbbcb5b4d5e1a68cb8719
전진호 <gino9940@gmail.com>
2023-12-05 23:21:04 +0900

    feat: add transition when slided

--- src/components/ChoiceSlider/ChoiceSlider.tsx
+++ src/components/ChoiceSlider/ChoiceSlider.tsx
@@ -6,48 +6,48 @@ import ChoiceSlide from '@components/ChoiceSlide/ChoiceSlide';
 
 interface ChoiceSliderProps {}
 
+const getScreenWidth = () => {
+  return window.innerWidth > 512 ? 512 : window.innerWidth;
+};
+
 const ChoiceSlider = () => {
+  const screenWidth = getScreenWidth();
+
   const controls = useAnimation();
 
   const variants = {
     A: {
-      translateX: 300,
+      translateX: screenWidth / 2 + 7.5 + 413,
       opacity: 0,
     },
     B: {
-      translateX: -800,
+      translateX: -(screenWidth / 2 + 7.5 + 413),
       opacity: 0,
     },
   };
 
   const handleDragEnd = (_: MouseEvent | TouchEvent | PointerEvent, info: PanInfo) => {
-    // if (info.velocity.x < 0 && info.offset.x < -(containerWidth / 2 + 7.5)) {
-    //   // B 슬라이드
-    //   controls.start('B');
-    //   setHasVoted(true);
-    // } else if (info.velocity.x > 0 && info.offset.x > containerWidth / 2 + 7.5) {
-    //   // A 슬라이드
-    //   controls.start('A');
-    //   setHasVoted(true);
-    // }
+    if (info.velocity.x < 0 && info.offset.x < -(screenWidth / 2 + 7.5)) {
+      // B 슬라이드
+      controls.start('B');
+      //   setHasVoted(true);
+    } else if (info.velocity.x > 0 && info.offset.x > screenWidth / 2 + 7.5) {
+      // A 슬라이드
+      controls.start('A');
+      //   setHasVoted(true);
+    }
   };
 
   return (
     <SelectContainer
       animate={controls}
       drag="x"
-      // initial={{ translateX: `${containerWidth / 2 - 420.5}px` }}
       onDragEnd={handleDragEnd}
       variants={variants}
       dragSnapToOrigin={true}
-      dragConstraints={
-        {
-          // left: -(containerWidth / 2 + 7.5), // -(413 + 15 - contain erWidth / 2 - 420.5)
-          // right: containerWidth / 2 + 7.5, // 413 + 15 - 236
-        }
-      }
       dragElastic={0.1}
       dragTransition={{ bounceStiffness: 200, bounceDamping: 20 }}
+      transition={{ ease: 'easeOut' }}
     >
       <ChoiceSlide side={'A'} />
       <ChoiceSlide side={'B'} />
