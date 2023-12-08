2bfa2e65fac466d8f8b54757ba586728bf78fa4d
전진호 <gino9940@gmail.com>
2023-12-08 17:11:30 +0900

    feat: add row, col common props

---
+++ src/components/commons/Flex/Flex.tsx
@@ -0,0 +1,63 @@
+import React from 'react';
+import styled, { css } from 'styled-components';
+
+interface FlexProps {
+  justifyContent?: React.CSSProperties['justifyContent'];
+  alignItems?: React.CSSProperties['alignItems'];
+  gap?: React.CSSProperties['gap'];
+  padding?: React.CSSProperties['padding'];
+  margin?: React.CSSProperties['margin'];
+  children: React.ReactNode;
+}
+
+export const Row = (props: FlexProps) => {
+  const { children, justifyContent = 'space-between', alignItems = 'center', ...rest } = props;
+
+  return (
+    <Flex flexDirection={'row'} justifyContent={justifyContent} alignItems={alignItems} {...rest}>
+      {children}
+    </Flex>
+  );
+};
+
+export const Col = (props: FlexProps) => {
+  const { children, justifyContent = 'space-between', alignItems = 'center', ...rest } = props;
+
+  return (
+    <Flex
+      flexDirection={'column'}
+      justifyContent={justifyContent}
+      alignItems={alignItems}
+      {...rest}
+    >
+      {children}
+    </Flex>
+  );
+};
+
+interface StyledFlexProps extends Omit<FlexProps, 'children'> {
+  flexDirection: React.CSSProperties['flexDirection'];
+}
+
+const Flex = styled.div<StyledFlexProps>`
+  display: flex;
+  flex-direction: ${(props) => props.flexDirection};
+  gap: ${(props) => props.gap};
+  align-items: ${(props) => props.alignItems};
+  justify-content: ${(props) => props.justifyContent};
+  ${({ gap }) =>
+    gap &&
+    css`
+      gap: ${gap};
+    `}
+  ${({ padding }) =>
+    padding &&
+    css`
+      padding: ${padding};
+    `}
+    ${({ margin }) =>
+    margin &&
+    css`
+      margin: ${margin};
+    `}
+`;
