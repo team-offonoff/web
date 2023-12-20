408b9c13578842699a1c0038063da96129576bfe
전진호 <gino9940@gmail.com>
2023-12-20 18:59:15 +0900

    feat: add some colors

--- src/assets/styles/theme.ts
+++ src/assets/styles/theme.ts
@@ -5,13 +5,18 @@ export const colors = {
   navy: '#242036',
   navy2: '#4D3B7C',
   A: '#D04376',
+  sub_A: '#DF8AA9',
   B: '#1498AA',
   black: '#000000',
   white: '#FFFFFF',
   white_20: 'rgba(255, 255, 255, 0.20)',
+  black_20: 'rgba(0, 0, 0, 0.20)',
   white_40: 'rgba(255, 255, 255, 0.40)',
+  black_40: 'rgba(0, 0, 0, 0.40)',
   white_60: 'rgba(255, 255, 255, 0.60)',
+  black_60: 'rgba(0, 0, 0, 0.60)',
   white_80: 'rgba(255, 255, 255, 0.80)',
+  black_80: 'rgba(0, 0, 0, 0.80)',
 };
 
 export const zIndex = {
