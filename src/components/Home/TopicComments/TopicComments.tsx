31e632517f8b46be17b0fdf7c2025e148ee68338
전진호 <gino9940@gmail.com>
2023-12-22 03:27:49 +0900

    feat: add TopicComment

--- src/components/Home/TopicComments/TopicComments.tsx
+++ src/components/Home/TopicComments/TopicComments.tsx
@@ -1,11 +1,23 @@
 import { InfiniteData } from '@tanstack/query-core';
 import React from 'react';
 
+import { Row } from '@components/commons/Flex/Flex';
+import Text from '@components/commons/Text/Text';
 import { CommentResponse } from '@interfaces/api/comment';
 
 import { PagingDataResponse } from '@interfaces/api';
 
+import { colors } from '@styles/theme';
+
 import Comment from '../Comment/Comment';
+
+import {
+  CommentInput,
+  CommentInputContainer,
+  CommentsContainer,
+  TopicCommentsContainer,
+  TopicCommentsHeader,
+} from './TopicComments.styles';
 
 interface TopicCommentsProps {
   topicId: number;
@@ -14,15 +26,30 @@ interface TopicCommentsProps {
 
 const TopicComments = ({ topicId, comments }: TopicCommentsProps) => {
   return (
-    <div>
-      {comments?.pages.map((group, i) => (
-        <React.Fragment key={i}>
-          {group.data.map((comment) => (
-            <Comment key={comment.commentId} comment={comment} />
-          ))}
-        </React.Fragment>
-      ))}
-    </div>
+    <TopicCommentsContainer>
+      <TopicCommentsHeader className="draggable">
+        <Row className="draggable">
+          <Text className="draggable" size={18} weight={500} color={colors.black}>
+            1천개
+          </Text>
+          <Text className="draggable" size={18} weight={500} color={colors.black_40}>
+            의 댓글
+          </Text>
+        </Row>
+      </TopicCommentsHeader>
+      <CommentsContainer>
+        {comments?.pages.map((group, i) => (
+          <React.Fragment key={i}>
+            {group.data.map((comment) => (
+              <Comment key={comment.commentId} comment={comment} />
+            ))}
+          </React.Fragment>
+        ))}
+      </CommentsContainer>
+      <CommentInputContainer>
+        <CommentInput type="text" placeholder="댓글 쓰기" />
+      </CommentInputContainer>
+    </TopicCommentsContainer>
   );
 };
 
