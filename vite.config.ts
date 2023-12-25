69363d6c237d30f4aeb98226b0257231755955c1
전진호 <gino9940@gmail.com>
2023-12-26 08:16:42 +0900

    feat: add pwa settings

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,45 @@
 import react from '@vitejs/plugin-react';
 import { defineConfig } from 'vite';
+import { VitePWA } from 'vite-plugin-pwa';
 import svgr from 'vite-plugin-svgr';
 import tsconfigPaths from 'vite-tsconfig-paths';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react(), svgr(), tsconfigPaths()],
+  plugins: [
+    react(),
+    svgr(),
+    tsconfigPaths(),
+    VitePWA({
+      registerType: 'autoUpdate',
+      devOptions: {
+        enabled: true,
+      },
+      manifest: {
+        icons: [
+          {
+            src: '/pwa-64x64.png',
+            type: 'image/png',
+            sizes: '64x64',
+          },
+          {
+            src: '/pwa-192x192.png',
+            type: 'image/png',
+            sizes: '192x192',
+          },
+          {
+            src: '/pwa-512x512.png',
+            type: 'image/png',
+            sizes: '512x512',
+          },
+          {
+            src: '/maskable-icon-512x512.png',
+            type: 'image/png',
+            sizes: '512x512',
+            purpose: 'maskable',
+          },
+        ],
+      },
+    }),
+  ],
 });
