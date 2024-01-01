24429e0021e1b1ba8f5e755d35979f5edef0e3a8
전진호 <gino9940@gmail.com>
2024-01-01 16:47:21 +0900

    fix: replace to useFormContext

--- src/components/commons/TextInput/TextInput.tsx
+++ src/components/commons/TextInput/TextInput.tsx
@@ -1,59 +1,18 @@
+/* eslint-disable @typescript-eslint/no-explicit-any */
 import React from 'react';
+import { RegisterOptions, useFormContext } from 'react-hook-form';
+import { ConfigKeys } from 'src/constants/form';
 import { styled } from 'styled-components';
 
 import { colors } from '@styles/theme';
 
 interface TextInputProps {
+  id: ConfigKeys;
+  options: RegisterOptions;
   placeholder: string;
-  value: string;
-  guideMessage?: string;
-  onChange: (event: React.ChangeEvent<HTMLInputElement>) => void;
+  left?: () => React.ReactNode;
   right?: () => React.ReactNode;
-  type?: 'default' | 'error' | 'success';
 }
-
-const TextInput = React.memo((props: TextInputProps) => {
-  const { placeholder, value, guideMessage, right, onChange, type = 'default' } = props;
-
-  let inputTheme;
-
-  switch (type) {
-    case 'default':
-      inputTheme = {
-        border: `1px solid ${colors.sub_purple}`,
-        backgroundColor: 'transparent',
-      };
-      break;
-    case 'success':
-      inputTheme = {
-        border: 'none',
-        backgroundColor: `${colors.navy2}`,
-      };
-      break;
-    case 'error':
-      inputTheme = {
-        border: `1px solid #BE28F3`,
-        backgroundColor: 'transparent',
-      };
-      break;
-  }
-
-  return (
-    <div>
-      <div>
-        <StyledInput
-          type="text"
-          placeholder={placeholder}
-          value={value}
-          onChange={onChange}
-          style={{ ...inputTheme }}
-        />
-        {right && <div>{right()}</div>}
-      </div>
-      {guideMessage && <div>{guideMessage}</div>}
-    </div>
-  );
-});
 
 const StyledInput = styled.input`
   padding: 14px 16px;
@@ -66,5 +25,51 @@ const StyledInput = styled.input`
     outline: none;
   }
 `;
+
+const TextInput = (props: TextInputProps) => {
+  const { id, options, placeholder, left, right } = props;
+  const {
+    register,
+    watch,
+    formState: { errors },
+  } = useFormContext();
+
+  const inputTheme = {
+    default: {
+      border: `1px solid ${colors.sub_purple}`,
+      backgroundColor: 'transparent',
+    },
+    success: {
+      border: 'none',
+      backgroundColor: `${colors.navy2}`,
+    },
+    error: {
+      border: `1px solid #BE28F3`,
+      backgroundColor: 'transparent',
+    },
+  };
+
+  return (
+    <div>
+      <div>
+        {left && <div>{left()}</div>}
+        <StyledInput
+          type="text"
+          style={
+            watch(id)?.length > 0
+              ? errors.root
+                ? inputTheme.error
+                : inputTheme.success
+              : inputTheme.default
+          }
+          placeholder={placeholder}
+          {...register(id, options)}
+        />
+        {right && <div>{right()}</div>}
+      </div>
+      {errors[id] && <div>{errors[id]?.message?.toString()}</div>}
+    </div>
+  );
+};
 
 export default TextInput;
--- src/routes/Auth/signup/정보입력.tsx
+++ src/routes/Auth/signup/정보입력.tsx
@@ -1,34 +1,34 @@
 import React from 'react';
+import { FormProvider, SubmitHandler, useForm } from 'react-hook-form';
+import { CONFIG } from 'src/constants/form';
 
 import TextInput from '@components/commons/TextInput/TextInput';
 
-import {
-  BirthInput,
-  Container,
-  GenderInput,
-  InputContainer,
-  JobInput,
-  NextButton,
-  NicknameInput,
-} from './정보입력.styles';
+import { Container, NextButton } from './정보입력.styles';
+
+export interface SignUpForm {
+  nickname: string;
+}
 
 const 정보입력 = () => {
-  const handleNextButton = () => {
-    //다음버튼 누르면
+  const methods = useForm<SignUpForm>({ mode: 'onChange' });
+
+  const handleSubmitForm: SubmitHandler<SignUpForm> = (data) => {
+    console.log(data);
   };
 
   return (
     <Container>
-      <InputContainer>
-        <TextInput
-          placeholder={''}
-          value={''}
-          onChange={function (event: React.ChangeEvent<HTMLInputElement>): void {
-            throw new Error('Function not implemented.');
-          }}
-        />
-      </InputContainer>
-      <NextButton onClick={handleNextButton}>다음</NextButton>
+      <FormProvider {...methods}>
+        <form onSubmit={methods.handleSubmit(handleSubmitForm)}>
+          <TextInput
+            id={'NICKNAME'}
+            options={CONFIG.NICKNAME.options}
+            placeholder={'한글, 영문, 숫자 최대 8자'}
+          />
+          <NextButton type={'submit'}>다음</NextButton>
+        </form>
+      </FormProvider>
     </Container>
   );
 };
