d110e98a0ae462cc4e52fd8d132dc53394f9ffac
전진호 <gino9940@gmail.com>
2024-01-01 17:56:44 +0900

    feat: add nickname progress

--- src/routes/Auth/signup/정보입력.tsx
+++ src/routes/Auth/signup/정보입력.tsx
@@ -2,12 +2,15 @@ import React from 'react';
 import { FormProvider, SubmitHandler, useForm } from 'react-hook-form';
 import { CONFIG, INPUT_TYPE } from 'src/constants/form';
 
+import Text from '@components/commons/Text/Text';
 import TextInput from '@components/commons/TextInput/TextInput';
+
+import { colors } from '@styles/theme';
 
 import { Container, NextButton } from './정보입력.styles';
 
 export interface SignUpForm {
-  nickname: string;
+  NICKNAME: string;
 }
 
 const 정보입력 = () => {
@@ -17,6 +20,10 @@ const 정보입력 = () => {
     console.log(data);
   };
 
+  const nicknameProgress = methods.watch(INPUT_TYPE.NICKNAME)
+    ? `${methods.watch(INPUT_TYPE.NICKNAME)?.length}/8`
+    : '';
+
   return (
     <Container>
       <FormProvider {...methods}>
@@ -25,6 +32,11 @@ const 정보입력 = () => {
             id={INPUT_TYPE.NICKNAME}
             options={CONFIG.NICKNAME.options}
             placeholder={'한글, 영문, 숫자 최대 8자'}
+            right={() => (
+              <Text size={14} weight={700} color={colors.sub_purple}>
+                {nicknameProgress}
+              </Text>
+            )}
           />
           <NextButton type={'submit'}>다음</NextButton>
         </form>
