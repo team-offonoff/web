282c6f481c09f206a6dc65f4e24b7a550f9ba8dc
전진호 <gino9940@gmail.com>
2024-01-02 15:20:26 +0900

    feat: add 정보입력 constants

---
+++ src/constants/signup.ts
@@ -0,0 +1,17 @@
+export const JOBS = [
+  { value: 'student', label: '학생' },
+  { value: 'developer', label: '개발자' },
+  { value: 'designer', label: '디자이너' },
+  { value: 'etc', label: '기타' },
+];
+
+export const GENDERS = [
+  {
+    label: '남성',
+    value: 'male',
+  },
+  {
+    label: '여성',
+    value: 'female',
+  },
+];
