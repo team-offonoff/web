5ef9cf55f471cf16b94c8fdf27cc150865b69f43
chaeyoung103 <thdcodud103@naver.com>
2024-01-11 22:34:42 +0900

    feat: 토픽 만들기 페이지 side에 따라 button ui 변경되는 로직 추가

--- src/routes/Topic/TopicCreate.sytles.tsx
+++ src/routes/Topic/TopicCreate.sytles.tsx
@@ -23,3 +23,50 @@ export const EmptyDiv = styled.div`
   width: 24px;
   height: 24px;
 `;
+
+export const HeaderCenterContainer = styled.div`
+  display: flex;
+  gap: 8px;
+  align-items: center;
+  justify-content: center;
+`;
+
+export const SideButton = styled.button<{ side: string | undefined }>`
+  position: relative;
+  display: flex;
+  gap: 3px;
+  align-items: center;
+  justify-content: center;
+  width: 90px;
+  height: 25px;
+  padding: 2px 10px;
+  background-color: ${(props) =>
+    props.side === 'A' ? 'rgb(208 67 118 / 20%)' : 'rgb(20 152 170 / 20%)'};
+  border-radius: 36px;
+`;
+
+export const SideChangeButton = styled.button<{ side: string | undefined; isHidden: boolean }>`
+  position: absolute;
+  bottom: -42px;
+  left: -9.5px;
+  z-index: 1;
+  display: flex;
+  gap: 1px;
+  align-items: center;
+  justify-content: center;
+  width: 154px;
+  height: 32px;
+  padding: 6px 20px;
+  visibility: ${(props) => (props.isHidden ? 'hidden' : 'visible')};
+  background-color: ${(props) =>
+    props.side === 'B' ? 'rgb(208 67 118 / 20%)' : 'rgb(20 152 170 / 20%)'};
+  border-radius: 50px;
+`;
+
+export const DownShevron = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: flex-start;
+  width: 14px;
+  height: 14px;
+`;
--- src/routes/Topic/TopicCreate.tsx
+++ src/routes/Topic/TopicCreate.tsx
@@ -1,20 +1,39 @@
 import React, { useState } from 'react';
-import { useNavigate } from 'react-router-dom';
+import { useNavigate, useParams } from 'react-router-dom';
 
-import CloseButton from '@components/commons/Header/CloseButton/CloseButton';
 import Layout from '@components/commons/Layout/Layout';
 import Text from '@components/commons/Text/Text';
 
 import { colors } from '@styles/theme';
 
-import { ALogoIcon, RightChevronIcon } from '@icons/index';
+import { DownChevronIcon, RightChevronIcon } from '@icons/index';
 
-import { BackButton } from './TopicCreate.sytles';
+import {
+  BackButton,
+  DownShevron,
+  HeaderCenterContainer,
+  SideButton,
+  SideChangeButton,
+} from './TopicCreate.sytles';
 import { Container, EmptyDiv } from './TopicSideSelection.styles';
 
 const TopicCreate = () => {
-  const [selected, setSelected] = useState<'A' | 'B' | null>(null);
   const navigate = useNavigate();
+  const { topicSide } = useParams();
+  const [isHidden, setIsHidden] = useState<boolean>(true);
+
+  const handleSideButtonClick = () => {
+    if (isHidden) {
+      setIsHidden(false);
+    } else {
+      setIsHidden(true);
+    }
+  };
+
+  const handleSideChangeButtonClick = (newtopicSideValue: string) => {
+    navigate(`/topics/create/${newtopicSideValue}`);
+  };
+
   return (
     <Layout
       hasBottomNavigation={false}
@@ -24,9 +43,31 @@ const TopicCreate = () => {
         </BackButton>
       )}
       HeaderCenter={() => (
-        <Text size={20} weight={600} color={colors.white}>
-          토픽 만들기
-        </Text>
+        <HeaderCenterContainer>
+          <Text size={20} weight={600} color={colors.white}>
+            토픽 만들기
+          </Text>
+          <SideButton side={topicSide} onClick={handleSideButtonClick}>
+            <Text size={15} weight={500} color={topicSide === 'A' ? colors.A : colors.B}>
+              {topicSide} 사이드
+            </Text>
+            <DownShevron>
+              <DownChevronIcon stroke={topicSide === 'A' ? colors.A : colors.B} />
+            </DownShevron>
+            <SideChangeButton
+              side={topicSide}
+              isHidden={isHidden}
+              onClick={() => handleSideChangeButtonClick(topicSide === 'A' ? 'B' : 'A')}
+            >
+              <Text size={14} weight={500} color={topicSide === 'A' ? colors.B : colors.A}>
+                {topicSide === 'A' ? 'B' : 'A'} 사이드
+              </Text>
+              <Text size={14} weight={500} color={colors.white}>
+                로 변경하기
+              </Text>
+            </SideChangeButton>
+          </SideButton>
+        </HeaderCenterContainer>
       )}
       HeaderRight={() => <EmptyDiv />}
     >
