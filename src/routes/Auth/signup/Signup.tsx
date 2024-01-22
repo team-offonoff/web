75ae65f160ddb4df4a96deae228fdf0055e0d88e
전진호 <gino9940@gmail.com>
2024-01-22 20:52:04 +0900

    fix: remove funnel from signup

--- src/routes/Auth/signup/Signup.tsx
+++ src/routes/Auth/signup/Signup.tsx
@@ -1,27 +1,106 @@
-import React, { useEffect, useState } from 'react';
-import { useLocation } from 'react-router-dom';
+import React, { useEffect } from 'react';
+import { FormProvider, SubmitHandler, useForm } from 'react-hook-form';
+import { CONFIG, INPUT_TYPE } from 'src/constants/form';
+import { GENDERS, JOBS } from 'src/constants/signup';
 
-import useFunnel from '@hooks/useFunnel/useFunnel';
+import { SingnUpRequestDTO, useSignup } from '@apis/oauth/signup';
+import { Col } from '@components/commons/Flex/Flex';
+import InputField from '@components/commons/InputField/InputField';
+import Layout from '@components/commons/Layout/Layout';
+import RadioInput from '@components/commons/RadioInput/RadioInput';
+import SelectInput from '@components/commons/SelectInput/SelectInput';
+import Text from '@components/commons/Text/Text';
+import TextInput from '@components/commons/TextInput/TextInput';
 
-import 가입성공 from './가입성공';
-import 정보입력 from './정보입력';
+import { colors } from '@styles/theme';
 
-const Signup = () => {
-  const steps = ['정보입력', '가입성공'];
+import { FormContainer, NextButton } from './Signup.styles';
 
-  const [Funnel, setStep] = useFunnel(steps);
-  const location = useLocation();
-  const [registerData, setRegisterData] = useState();
+type SignupForm = Omit<SingnUpRequestDTO, 'memberId'>;
+
+const MAX_NICKNAME_LENGTH = 8;
+
+const Signup = ({ memberId }: { memberId: number }) => {
+  const methods = useForm<Omit<SingnUpRequestDTO, 'memberId'>>({ mode: 'onChange' });
+  const signupMutation = useSignup();
+
+  const birthdayInput = methods.watch(INPUT_TYPE.BIRTHDAY);
+  const nicknameProgress = methods.watch(INPUT_TYPE.NICKNAME)
+    ? `${methods.watch(INPUT_TYPE.NICKNAME)?.length}/${MAX_NICKNAME_LENGTH}`
+    : '';
+
+  const handleBirthdayInputKeyDown = (e: React.KeyboardEvent<HTMLInputElement>) => {
+    if (e.key === 'Backspace' && (birthdayInput?.length === 6 || birthdayInput?.length === 9)) {
+      methods.setValue(INPUT_TYPE.BIRTHDAY, birthdayInput.slice(0, -2));
+    }
+    if (e.key === 'Backspace' && (birthdayInput?.length === 5 || birthdayInput?.length === 8)) {
+      methods.setValue(INPUT_TYPE.BIRTHDAY, birthdayInput.slice(0, -1));
+    }
+  };
+
+  const handleSubmitForm: SubmitHandler<SignupForm> = (data) => {
+    signupMutation.mutate({ ...data, memberId });
+  };
+
+  useEffect(() => {
+    if (birthdayInput?.length === 4 || birthdayInput?.length === 7) {
+      methods.setValue(INPUT_TYPE.BIRTHDAY, birthdayInput + '/');
+    }
+  }, [birthdayInput, methods]);
 
   return (
-    <Funnel>
-      <Funnel.Step name="정보입력">
-        <정보입력 memberId={location.state.memberId} />
-      </Funnel.Step>
-      <Funnel.Step name="가입성공">
-        <가입성공 />
-      </Funnel.Step>
-    </Funnel>
+    <Layout
+      hasBottomNavigation={false}
+      HeaderCenter={() => (
+        <Text size={20} weight={700} color={colors.white}>
+          회원정보 입력
+        </Text>
+      )}
+    >
+      <FormProvider {...methods}>
+        <FormContainer onSubmit={methods.handleSubmit(handleSubmitForm)}>
+          <Col gap={48}>
+            <InputField label="AB에서 사용할 닉네임을 정해주세요.">
+              <TextInput
+                id={INPUT_TYPE.NICKNAME}
+                options={CONFIG.NICKNAME.options}
+                placeholder={'한글, 영문, 숫자 최대 8자'}
+                right={() => (
+                  <Text size={14} weight={700} color={colors.purple}>
+                    {nicknameProgress}
+                  </Text>
+                )}
+              />
+            </InputField>
+            <InputField label="생년월일을 입력해주세요.">
+              <TextInput
+                id={INPUT_TYPE.BIRTHDAY}
+                type={'tel'}
+                options={CONFIG.BIRTHDAY.options}
+                placeholder={'YYYY/MM/DD'}
+                onKeyDown={handleBirthdayInputKeyDown}
+              />
+            </InputField>
+            <InputField label="성별을 선택해주세요.">
+              <RadioInput
+                id={INPUT_TYPE.GENDER}
+                options={CONFIG.GENDER.options}
+                radioOptions={GENDERS}
+              />
+            </InputField>
+            <InputField label="직업을 선택해주세요.">
+              <SelectInput
+                id={INPUT_TYPE.JOB}
+                options={CONFIG.JOB.options}
+                placeholder="직업 선택하기"
+                selectOptions={JOBS}
+              />
+            </InputField>
+          </Col>
+          <NextButton type={'submit'}>다음</NextButton>
+        </FormContainer>
+      </FormProvider>
+    </Layout>
   );
 };
 
