5fa47975a44ceeca2e5ad85482476a77c0a48f2c
전진호 <gino9940@gmail.com>
2024-01-30 20:32:22 +0900

    feat: add ToggleSwitch

---
+++ src/components/commons/ToggleSwitch/SwitchItem.tsx
@@ -0,0 +1,35 @@
+import { InputHTMLAttributes, Ref, forwardRef } from 'react';
+import { useUID } from 'react-uid';
+
+import { OptionContainer, OptionInput, OptionLabel } from './ToggleSwitch.styles';
+
+function SwitchItem(
+  {
+    className,
+    children,
+    checked,
+    id: elementId,
+    ...rest
+  }: Omit<InputHTMLAttributes<HTMLInputElement>, 'type'>,
+  forwardedRef: Ref<HTMLInputElement>
+) {
+  const id = useUID();
+
+  return (
+    <OptionContainer>
+      <OptionInput
+        className="visually-hidden radio-option__input"
+        id={id}
+        type="radio"
+        checked={checked}
+        ref={forwardedRef}
+        {...rest}
+      />
+      <OptionLabel className="radio-option__label" htmlFor={id}>
+        <span>{children}</span>
+      </OptionLabel>
+    </OptionContainer>
+  );
+}
+
+export default forwardRef(SwitchItem);
