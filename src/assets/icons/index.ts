9635ad1f000f8ab1e64785f9509a64eab0507e8a
chaeyoung103 <thdcodud103@naver.com>
2024-02-02 16:02:08 +0900

    feat: add delete icon

--- src/assets/icons/index.ts
+++ src/assets/icons/index.ts
@@ -10,6 +10,7 @@ import CheckIcon from './check.svg?react';
 import ClockIcon from './clock.svg?react';
 import CloseIcon from './close.svg?react';
 import CommentIcon from './comment.svg?react';
+import DeleteIcon from './delete.svg?react';
 import DownChevronIcon from './down-chevron.svg?react';
 import GoogleIcon from './google.svg?react';
 import HideIcon from './hide.svg?react';
@@ -70,4 +71,5 @@ export {
   SelectedTextIcon,
   ImageIcon,
   SelectedImageIcon,
+  DeleteIcon,
 };
