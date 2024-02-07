9252b80ed5eb4e0272556f722cc919c7062058bd
chaeyoung103 <thdcodud103@naver.com>
2024-02-07 15:44:12 +0900

    feat: 토픽생성 api 구축

--- src/apis/topic/useTopics.ts
+++ src/apis/topic/useTopics.ts
@@ -1,12 +1,26 @@
-import { useQuery } from '@tanstack/react-query';
+import { useMutation, useQuery } from '@tanstack/react-query';
 
-import { TopicResponse } from '@interfaces/api/topic';
+import { CHOICE_OPTIONS, ChoiceContent, TopicResponse } from '@interfaces/api/topic';
 
 import { PagingDataResponse } from '@interfaces/api';
 
 import client from '@apis/fetch';
 
 export const TOPIC_KEY = 'topics';
+
+interface Choice {
+  choiceId: number;
+  choiceContentRequest: ChoiceContent;
+  choiceOption: typeof CHOICE_OPTIONS.CHOICE_A | typeof CHOICE_OPTIONS.CHOICE_B;
+}
+
+interface TopicCreateRequestDTO {
+  side: string;
+  keywordName: string;
+  title: string;
+  choices: Choice[];
+  deadline: number;
+}
 
 const getTopics = () => {
   return client.get<PagingDataResponse<TopicResponse>>('/topics/info/voting');
@@ -16,4 +30,17 @@ const useTopics = () => {
   return useQuery({ queryKey: [TOPIC_KEY], queryFn: getTopics });
 };
 
+const createTopics = (req: TopicCreateRequestDTO) => {
+  return client.post({
+    path: `/topics`,
+    body: req,
+  });
+};
+
+const useCreateTopics = () => {
+  return useMutation({ mutationFn: createTopics });
+};
+
 export default useTopics;
+
+export { useCreateTopics };
--- src/constants/topic.ts
+++ src/constants/topic.ts
@@ -5,11 +5,20 @@ interface TopicDeadline {
 
 export const TOPIC_DEADLINES: TopicDeadline[] = Array.from({ length: 24 }, (_, i) => {
   const hoursToAdd = i + 1;
-  const date = new Date();
-  date.setHours(date.getHours() + hoursToAdd);
 
   return {
     label: `${hoursToAdd}시간`,
-    value: Math.floor(date.getTime() / 1000),
+    value: i + 1,
   };
 });
+
+// export const TOPIC_DEADLINES: TopicDeadline[] = Array.from({ length: 24 }, (_, i) => {
+//     const hoursToAdd = i + 1;
+//     const date = new Date();
+//     date.setHours(date.getHours() + hoursToAdd);
+
+//     return {
+//       label: `${hoursToAdd}시간`,
+//       value: Math.floor(date.getTime() / 1000),
+//     };
+//   });
