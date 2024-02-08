f89216433fad9f86566cdfbca4bbdd9d4f9f43d1
chaeyoung103 <thdcodud103@naver.com>
2024-02-08 15:14:38 +0900

    fix: 로고색상 변경 #169

--- src/routes/Topic/TopicSideSelection.tsx
+++ src/routes/Topic/TopicSideSelection.tsx
@@ -74,7 +74,7 @@ const TopicSideSelection = () => {
             </ADescription>
           </AButton>
           <BButton selected={selected} onClick={handleBButtonClick}>
-            <BLogoIcon />
+            <BLogoIcon fill={colors.B} />
             <BDescription selected={selected}>
               Business <br /> Side
             </BDescription>
