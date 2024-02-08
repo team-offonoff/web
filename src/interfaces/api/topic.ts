a8912b7a46e199689fce0b0060336cbf7d0d81ec
전진호 <gino9940@gmail.com>
2024-02-08 22:38:36 +0900

    fix: make TopicResponse's field nullable

--- src/interfaces/api/topic.ts
+++ src/interfaces/api/topic.ts
@@ -7,10 +7,10 @@ interface TopicResponse {
   topicId: number;
   topicSide: string;
   topicTitle: string;
-  deadline: number; // 1702914494
+  deadline: number | null; // 1702914494
   voteCount: number;
-  topicContent: string; // TBD
-  keyword: Keyword;
+  topicContent: string | null;
+  keyword: Keyword | null;
   choices: Choice[];
   author: Author;
   selectedOption: typeof CHOICE_OPTIONS.CHOICE_A | typeof CHOICE_OPTIONS.CHOICE_B | null;
