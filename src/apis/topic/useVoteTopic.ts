c13fa526094b5250eefd77b51104e83a126b7261
전진호 <gino9940@gmail.com>
2024-02-09 22:18:30 +0900

    feat: add rea params to set exact query data

--- src/apis/topic/useVoteTopic.ts
+++ src/apis/topic/useVoteTopic.ts
@@ -7,7 +7,7 @@ import { PagingDataResponse } from '@interfaces/api';
 
 import client from '@apis/fetch';
 
-import { TOPIC_KEY } from './useTopics';
+import { TOPIC_KEY, TopicsRequestDTO } from './useTopics';
 
 interface VoteTopicRequest {
   topicId: number;
@@ -25,22 +25,23 @@ const voteTopic = ({ topicId, choiceOption, votedAt }: VoteTopicRequest) => {
   });
 };
 
-const useVoteTopic = () => {
+const useVoteTopic = (req?: TopicsRequestDTO) => {
   const queryClient = useQueryClient();
 
   return useMutation({
     mutationFn: (params: VoteTopicRequest) => voteTopic(params),
     onSuccess: (data, variables: VoteTopicRequest) => {
-      queryClient.setQueryData([TOPIC_KEY], (prev: PagingDataResponse<TopicResponse>) => {
-        return {
-          data: prev.data.map((topic) => {
-            return topic.topicId === variables.topicId
-              ? { ...topic, selectedOption: variables.choiceOption }
-              : { ...topic };
-          }),
-          pageInfo: prev.pageInfo,
-        };
-      });
+      queryClient.setQueryData(
+        [TOPIC_KEY, ...(req ? [req] : [])],
+        (prev: PagingDataResponse<TopicResponse>) => {
+          return {
+            data: prev.data.map((topic) => {
+              return topic.topicId === variables.topicId ? { ...data.topic } : { ...topic };
+            }),
+            pageInfo: prev.pageInfo,
+          };
+        }
+      );
     },
   });
 };
