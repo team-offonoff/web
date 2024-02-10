16965dd5f3bfe84672c6d4b99e69e06d39c522fb
전진호 <gino9940@gmail.com>
2024-02-11 01:21:26 +0900

    fix: change login and add relogin

--- src/store/auth.ts
+++ src/store/auth.ts
@@ -3,30 +3,51 @@ import { persist } from 'zustand/middleware';
 
 import { OAuthResponse } from '@interfaces/api/oauth';
 
-import { ACCESS_TOKEN } from '@constants/localStorage';
+import client from '@apis/fetch';
 
 interface AuthState {
   isLoggedIn: boolean;
   memberId: null | OAuthResponse['memberId'];
+  refreshToken?: string;
 }
 
 interface AuthAction {
-  login: (memberId: OAuthResponse['memberId']) => boolean;
+  login: (acessToken: string, refreshToken: string, memberId: number) => void;
+  reLogin: () => Promise<boolean>;
   logout: () => void;
 }
 
 export const useAuthStore = create(
   persist<AuthState & AuthAction>(
-    (set) => ({
+    (set, get) => ({
       isLoggedIn: false,
       memberId: null,
-      login: (memberId: OAuthResponse['memberId']) => {
-        const userLocalStorage = localStorage.getItem(ACCESS_TOKEN);
-        if (userLocalStorage) {
-          set({ isLoggedIn: true, memberId: memberId });
-          return true;
-        } else {
-          return false;
+      login: (accessToken, refreshToken, memberId) => {
+        set({ refreshToken, memberId, isLoggedIn: true });
+        client.setAccessToken(accessToken);
+      },
+      reLogin: async () => {
+        const refreshToken = get().refreshToken;
+
+        if (!refreshToken) {
+          set({ isLoggedIn: false });
+          throw new Error('저장되어 있는 refreshToken이 없습니다.');
+        }
+
+        try {
+          const response = await client.post<OAuthResponse>({
+            path: '/auth/tokens',
+            body: {
+              refresh_token: refreshToken,
+            },
+          });
+          const { refreshToken: newRefreshToken, memberId, accessToken } = response;
+          set({ refreshToken: newRefreshToken, isLoggedIn: true, memberId });
+          client.setAccessToken(accessToken);
+          return get().isLoggedIn;
+        } catch (e) {
+          set({ isLoggedIn: false });
+          throw new Error('만료된 refreshToken입니다.');
         }
       },
       logout: () => {
