196a01136bfe5cb2be06e935ce8dda9b2adde2d9
전진호 <gino9940@gmail.com>
2024-02-18 23:38:36 +0900

    feat: add useOAuth to authroize commonly

---
+++ src/hooks/useOAuth/useOAuth.ts
@@ -0,0 +1,53 @@
+import { useNavigate } from 'react-router-dom';
+
+import { useAuthStore } from '@store/auth';
+
+import { ResponseError } from '@apis/fetch';
+import { OAuthLogin } from '@apis/oauth';
+
+type Platforms = 'kakao' | 'google' | 'apple';
+
+interface UseOAuthProps {
+  type: Platforms;
+}
+
+export default function useOAuth({ type }: UseOAuthProps) {
+  const navigate = useNavigate();
+  const login = useAuthStore((store) => store.login);
+
+  function getCode() {
+    if (type === 'kakao') {
+      return new URL(window.location.href).searchParams.get('code');
+    } else if (type === 'google') {
+      const code = new URL(window.location.href).searchParams.get('code') || '';
+      return decodeURI(code);
+    } else {
+      return 'apple';
+    }
+  }
+
+  async function handleLogin(code: string) {
+    try {
+      const response = await OAuthLogin(type, code);
+      if (response.newMember) {
+        navigate(`/signup`, {
+          state: { memberId: response.memberId },
+        });
+      } else {
+        login(response.accessToken, response.refreshToken, response.memberId);
+        navigate('/');
+      }
+    } catch (err) {
+      if (err instanceof ResponseError) {
+        if (err.errorData.abCode === 'ILLEGAL_JOIN_STATUS') {
+          navigate(`/signup`, {
+            state: { memberId: err.errorData.errorContent.payload },
+          });
+          return;
+        }
+      }
+    }
+  }
+
+  return { getCode, handleLogin };
+}
