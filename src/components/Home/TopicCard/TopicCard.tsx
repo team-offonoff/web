d08b6803296131974296079d03da8d379096136f
전진호 <gino9940@gmail.com>
2024-02-19 14:45:39 +0900

    fix: check if member created topic

--- src/components/Home/TopicCard/TopicCard.tsx
+++ src/components/Home/TopicCard/TopicCard.tsx
@@ -15,6 +15,8 @@ import useBottomSheet from '@hooks/useBottomSheet/useBottomSheet';
 import { LatestComment } from '@interfaces/api/comment';
 import { Choice, TopicResponse } from '@interfaces/api/topic';
 
+import { useAuthStore } from '@store/auth';
+
 import { colors } from '@styles/theme';
 
 import { LeftDoubleArrowIcon, RightDoubleArrowIcon } from '@icons/index';
@@ -38,12 +40,15 @@ interface TopicCardProps {
 
 const TopicCard = ({ topic }: TopicCardProps) => {
   const [, setSearchParams] = useSearchParams();
+  const memberId = useAuthStore((store) => store.memberId);
+  const isMyTopic = topic.author.id === memberId;
+
   const swiperSlide = useSwiperSlide();
   const { BottomSheet: CommentSheet, toggleSheet } = useBottomSheet({});
   const voteMutation = useVoteTopic();
   const { data: latestCommentData, isSuccess } = useLatestComment(
     topic.topicId,
-    topic.selectedOption !== null
+    topic.selectedOption !== null || isMyTopic
   );
   const [latestComment, setLatestComment] = useState<LatestComment | undefined>();
 
@@ -60,12 +65,13 @@ const TopicCard = ({ topic }: TopicCardProps) => {
 
   useEffect(() => {
     if (isSuccess) {
+      console.log('🚀 ~ useEffect ~ latestCommentData:', latestCommentData);
       setLatestComment(latestCommentData.data[0] as LatestComment);
     }
   }, [isSuccess]);
 
   const handleOnClickCommentBox = () => {
-    if (topic.selectedOption !== null) {
+    if (isMyTopic || topic.selectedOption !== null) {
       toggleSheet();
     }
   };
@@ -137,7 +143,7 @@ const TopicCard = ({ topic }: TopicCardProps) => {
         </SelectTextContainer>
         <CommentBox
           side={topic.topicSide}
-          hasVoted={topic.selectedOption !== null}
+          hasVoted={topic.selectedOption !== null || isMyTopic}
           topicId={topic.topicId}
           commentCount={0}
           voteCount={0}
