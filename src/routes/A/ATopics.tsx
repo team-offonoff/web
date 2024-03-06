8d99997c06a6b35eea3c429dcef0c48bbac8f72a
전진호 <gino9940@gmail.com>
2024-03-07 01:41:38 +0900

    feat: add useTrendingTopics

--- src/routes/A/ATopics.tsx
+++ src/routes/A/ATopics.tsx
@@ -1,13 +1,12 @@
-import { ChangeEvent, useState } from 'react';
+import { useState } from 'react';
 
-import useTopics from '@apis/topic/useTopics';
+import useTopics, { useTrendingTopics } from '@apis/topic/useTopics';
 import useVoteTopic from '@apis/topic/useVoteTopic';
 import ATopicCard from '@components/A/ATopicCard';
 import { Col, Row } from '@components/commons/Flex/Flex';
 import Layout from '@components/commons/Layout/Layout';
 import Text from '@components/commons/Text/Text';
 import { Toast } from '@components/commons/Toast/Toast';
-import ToggleSwitch from '@components/commons/ToggleSwitch/ToggleSwitch';
 
 import { colors } from '@styles/theme';
 
@@ -18,17 +17,15 @@ import { ResponseError } from '@apis/fetch';
 import { Container } from './ATopics.styles';
 
 const ATopics = () => {
-  const { data } = useTopics({ side: 'TOPIC_A', sort: 'createdAt,DESC' });
+  const { data: topicPages } = useTopics({ side: 'TOPIC_A', sort: 'createdAt,DESC' });
+  const { data: trendingTopicPages } = useTrendingTopics();
+
   const voteMutation = useVoteTopic({ side: 'TOPIC_A', sort: 'createdAt,DESC' });
-  // const [topicFilter, setTopicFilter] = useState('진행중');
   const [isMineOnly, setIsMineOnly] = useState(false);
   const [isLatest, setIsLatest] = useState(true);
 
-  const topics = data?.pages.flatMap((page) => page.data);
-
-  // const handleTopicStatusChange = (e: ChangeEvent<HTMLInputElement>) => {
-  //   setTopicFilter(e.target.value);
-  // };
+  const topics = topicPages?.pages.flatMap((page) => page.data);
+  const trendingTopics = trendingTopicPages?.pages.flatMap((page) => page.data);
 
   const handleVote = async (topicId: number, side: 'CHOICE_A' | 'CHOICE_B') => {
     try {
@@ -48,20 +45,6 @@ const ATopics = () => {
     <Layout
       hasBottomNavigation
       HeaderLeft={<ALogoIcon width={30} height={30} fill={colors.white} />}
-      // HeaderCenter={
-      //   <ToggleSwitch value={topicFilter} onChange={handleTopicStatusChange}>
-      //     <ToggleSwitch.Option value={'진행중'}>
-      //       <Text size={15} weight={500} color={'inherit'}>
-      //         진행중
-      //       </Text>
-      //     </ToggleSwitch.Option>
-      //     <ToggleSwitch.Option value={'종료된'}>
-      //       <Text size={15} weight={500} color={'inherit'}>
-      //         종료된
-      //       </Text>
-      //     </ToggleSwitch.Option>
-      //   </ToggleSwitch>
-      // }
     >
       <Container>
         <Row justifyContent={'flex-end'} gap={12} padding="15px 20px">
@@ -92,7 +75,17 @@ const ATopics = () => {
         </Row>
         <Col style={{ backgroundColor: 'inherit', paddingBottom: 100 }}>
           {topics?.map((topic) => {
-            return <ATopicCard key={topic.topicId} topic={topic} onVote={handleVote} />;
+            const isTrending = trendingTopics?.some(
+              (trendingTopic) => trendingTopic.topicId === topic.topicId
+            );
+            return (
+              <ATopicCard
+                key={topic.topicId}
+                topic={topic}
+                onVote={handleVote}
+                isTrending={isTrending}
+              />
+            );
           })}
         </Col>
       </Container>
