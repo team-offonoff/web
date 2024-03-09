7756aa4a04cfedf7a6bb2364a387e721b4f0e1d8
전진호 <gino9940@gmail.com>
2024-03-10 01:50:51 +0900

    feat: add read notification

--- src/apis/notification/useNotifications.ts
+++ src/apis/notification/useNotifications.ts
@@ -1,21 +1,28 @@
-import { useQuery } from '@tanstack/react-query';
+import { useMutation, useQuery, useQueryClient } from '@tanstack/react-query';
+
+import { NotificationResponse } from '@interfaces/api/notification';
 
 import client from '@apis/fetch';
 
 export const NOTICE_KEY = 'notifications';
 
 const getNotifications = () => {
-  return client.get('/notifications');
+  return client.get<NotificationResponse[]>('/notifications');
 };
 
 const getUnreadNotifications = () => {
-  return client.get('/notifications/counts/unchecked');
+  return client.get<number>('/notifications/counts/unread ');
+};
+
+const readNotification = (id: number) => {
+  return client.post({ path: `/notifications/${id}/read`, body: {} });
 };
 
 const useNotifications = () => {
   return useQuery({
     queryKey: [NOTICE_KEY],
     queryFn: getNotifications,
+    refetchOnMount: true,
   });
 };
 
@@ -23,7 +30,21 @@ const useUnreadNotifications = () => {
   return useQuery({
     queryKey: [NOTICE_KEY, 'unread'],
     queryFn: getUnreadNotifications,
+    refetchOnMount: true,
   });
 };
 
-export { useNotifications, useUnreadNotifications };
+const useReadNotification = (notificationId: number) => {
+  const queryClient = useQueryClient();
+
+  return useMutation({
+    mutationFn: () => readNotification(notificationId),
+    onMutate: () => {
+      queryClient.setQueryData([NOTICE_KEY], (prev: NotificationResponse[]) =>
+        prev.map((item) => (item.id === notificationId ? { ...item, isRead: true } : item))
+      );
+    },
+  });
+};
+
+export { useNotifications, useUnreadNotifications, useReadNotification };
